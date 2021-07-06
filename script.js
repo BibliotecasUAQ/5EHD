3dd6069ad42dae6b2b7c687bd5abdc671edeb0c2
dgbsdi <sistemas.bib@uaq.edu.mx>
2021-07-06 18:14:53 -0500

    comentarios de la junta

--- script.js
+++ script.js
@@ -16,24 +16,24 @@ $(document).ready(function() {
     window.clearTimeout(autoSlideTimeout);
     $slider.css("transform", "translateX(0%)");
     $slideBGs.css("transform", "translateX(0%)");
-  }
-  $(document).on("click", ".inicio", function() {
-    curSlide = $(this).data("page");
-    deslizar1();
+    }
+    $(document).on("click", ".inicio", function() {
+      curSlide = $(this).data("page");
+      deslizar1();
   });
-
+ 
   function deslizar2(instant) {
     if (!instant) {
       animating = true;
       $slider.addClass("animating");
       $(".slide").removeClass("active");
-      $(".slide-1").addClass("active");
+      $(".slide-2").addClass("active");
     }
     window.clearTimeout(autoSlideTimeout);
     $slider.css("transform", "translateX(-100%)");
     $slideBGs.css("transform", "translateX(-100%)");
   }
-  $(document).on("click", ".cambio", function() {
+  $(document).on("click", ".cambio2", function() {
     curSlide = $(this).data("page");
     deslizar2();
   });
@@ -43,48 +43,14 @@ $(document).ready(function() {
       animating = true;
       $slider.addClass("animating");
       $(".slide").removeClass("active");
-      $(".slide-2").addClass("active");
+      $(".slide-3").addClass("active");
     }
     window.clearTimeout(autoSlideTimeout);
     $slider.css("transform", "translateX(-200%)");
     $slideBGs.css("transform", "translateX(-200%)");
   }
-  $(document).on("click", ".cambio2", function() {
+  $(document).on("click", ".cambio3", function() {
     curSlide = $(this).data("page");
     deslizar3();
   });
-
-  function deslizar4(instant) {
-    if (!instant) {
-      animating = true;
-      $slider.addClass("animating");
-      $(".slide").removeClass("active");
-      $(".slide-3").addClass("active");
-    }
-    window.clearTimeout(autoSlideTimeout);
-    $slider.css("transform", "translateX(-300%)");
-    $slideBGs.css("transform", "translateX(-300%)");
-  }
-  $(document).on("click", ".cambio3", function() {
-    curSlide = $(this).data("page");
-    deslizar4();
-  });
-
-  function deslizar5(instant) {
-    if (!instant) {
-      animating = true;
-      $slider.addClass("animating");
-      $(".slide").removeClass("active");
-      $(".slide-4").addClass("active");
-    }
-    window.clearTimeout(autoSlideTimeout);
-    $slider.css("transform", "translateX(-400%)");
-    $slideBGs.css("transform", "translateX(-400%)");
-  }
-  $(document).on("click", ".cambio4", function() {
-    curSlide = $(this).data("page");
-    deslizar5();
-  });
-
-
 });
